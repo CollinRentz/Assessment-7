4a77417d85a3df5eab9faf673da4f4f5f365d77d
Collin Rentz <rentzcollin@gmail.com>
2022-10-29 15:37:00 -0600

    finished

--- cs/code.js
+++ cs/code.js
@@ -1,3 +1,5 @@
+// 1
+
 function addToZero(numbers, sum) {
     let array = [];
     for (let i = 0; i < numbers.length; i++) {
@@ -24,3 +26,69 @@ function addToZero(numbers, sum) {
   array = addToZero([1, 2, 3, -2], 0);
   console.log(array);
   // -> True
+
+
+  console.log("------------------------------");
+// 2
+
+  function hasUniqueChars(str) {
+    for(let i = 0; i < str.length; i++)
+        for(let j = i + 1; j < str.length; j++)
+            if (str[i] == str[j])
+                return false;
+    return true;
+}
+
+console.log(hasUniqueChars("Monday"));
+// -> True
+
+console.log(hasUniqueChars("Moonday"));
+// -> False
+
+console.log("------------------------------");
+// 3
+
+const isPangram = (str = '') => {
+    str = str.toLowerCase();
+    const alphabets = 'abcdefghijklmnopqrstuvwxyz';
+    const { length } = str;
+    const alphaArr = alphabets.split('');
+    for(let i = 0; i < length; i++){
+       const el = str[i];
+       const index = alphaArr.indexOf(el);
+       if(index !== -1){
+          alphaArr.splice(index, 1);
+       };
+    };
+    return !alphaArr.length;
+ };
+
+ console.log(isPangram("The quick brown fox jumps over the lazy dog!"));
+// -> True
+
+console.log(isPangram("I like cats, but not mice"));
+// -> False
+
+console.log("------------------------------");
+// 4
+
+function findLongestWord(string) {
+    let str = string;
+    let longest = 0;
+    let word = null;
+    for (var i = 0; i < str.length; i++) {
+        if (longest < str[i].length) {
+            longest = str[i].length;
+            word = str[i];
+        }
+    }
+    return word.length;
+}
+
+console.log(findLongestWord(["hi", "hello"]));
+// -> 5
+
+console.log(findLongestWord(["The", "quick", "brown", "fox", "jumped", "over", "the", "lazy", "dog"]));
+// -> 6
+
+console.log("------------------------------");
